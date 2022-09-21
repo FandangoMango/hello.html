54107b7ab6adf2c1d2250b64187e7af0a65e696a
FandangoMango <95419835+FandangoMango@users.noreply.github.com>
2022-09-21 19:42:34 +0200

    Add files via upload

---
+++ web_server/target/debug/deps/web_server.d
@@ -0,0 +1,5 @@
+C:\Users\mathi\rust\web_server\target\debug\deps\web_server.exe: src\main.rs
+
+C:\Users\mathi\rust\web_server\target\debug\deps\web_server.d: src\main.rs
+
+src\main.rs:
---
+++ web_server/target/debug/web_server.d
@@ -0,0 +1,1 @@
+C:\Users\mathi\rust\web_server\target\debug\web_server.exe: C:\Users\mathi\rust\web_server\src\main.rs
